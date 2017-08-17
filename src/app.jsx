759551ada23dd2469cd901daf59edde1bccec067
Djordje Grabovicanovic <djole@darkice>
2017-08-17 14:52:24 +0200

    Added landing page.

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'react-bootstrap';
-const buttonsInstance = (
-  <Button>Click me!</Button>
+
+function SmestajKladovo () {
+return (
+  <div className="container">
+    <Search />
+    <Browse />
+    <LogIn />
+  </div>
 );
-ReactDOM.render(buttonsInstance, document.getElementById('here'));
+}
+
+class Search extends React.Component {
+  render() {
+    return (<div><a className="search" href="#">Search</a></div>);
+  }
+}
+
+class Browse extends React.Component {
+  render() {
+    return (<div><a className="browse" href="#">Browse</a></div>);
+  }
+}
+
+class LogIn extends React.Component {
+  render() {
+    return (
+<div className="login row">
+<div className="col-6">
+    <form id="loginform">
+      <div className="form-group">
+        <div className="row">
+          <div className="mr-3"><label>Username</label><input className="form-control" type="text" id="username" placeholder="User Name" /></div>
+        </div>
+        <div className="row">
+          <div className="mr-3"><label>Password</label><input className="form-control" type="password" id="password" /></div>
+        </div>
+        <div className="row">
+          <button className="btn btn-primary" type="submit">Log In</button>
+        </div>
+      </div>
+    </form>
+    </div>
+</div>);
+  }
+}
+
+ReactDOM.render(
+  <SmestajKladovo />,
+  document.getElementById('content')
+);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import App from './app.jsx';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import bootstrapTheme from 'bootstrap/dist/css/bootstrap-theme.css';
